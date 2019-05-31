b4b2d731ddabae66144d86a284121086c570af46
Genghisg <grover.cn@gmail.com>
2019-05-30 23:57:54 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $botToken = "740210909:AAHqnF1gkdrD_1PXibnzd7W4lpn7M8ixDhk";
 
-$sitioweb = "https://api.telegram.org/bot".$botToken;
+$website = "https://api.telegram.org/bot".$botToken;
 $update = file_get_contents("php://input");
 $update = json_decode($update, TRUE);
 
