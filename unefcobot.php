9702da76946a2185506cdc8a8fecbc02bf54f03d
Genghisg <grover.cn@gmail.com>
2019-05-31 02:31:42 -0400

    Create unefcobot.php

---
+++ unefcobot.php
@@ -0,0 +1,32 @@
+<?php
+
+//echo "Hola como están, aquí desde Bolivia";
+$botToken = "740210909:AAHqnF1gkdrD_1PXibnzd7W4lpn7M8ixDhk";
+
+$website = "https://api.telegram.org/bot".$botToken;
+
+$update = file_get_contents($website."/getupdates");
+
+print_r($update);
+/*
+$update = json_decode($update, TRUE);
+
+$chatId = $update["message"]["chat"]["id"];
+$chatType = $update["message"]["chat"]["type"];
+
+$message = $update["message"]["text"];
+
+switch ($message) {
+    case '/ayuda':
+        $response = "Le ayudaré en lo que pueda";
+        sendMessage($chatId, $response);
+        break;
+    default:
+        break;
+}
+*/
+function sendMessage($chatId, $message){
+    $url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+    file_get_contents($url);
+}
+?>
